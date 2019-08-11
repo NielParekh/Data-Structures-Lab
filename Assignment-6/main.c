697405e581a72c0e45c22a9042b0f8b01538c662
MaheshBharadwaj <maheshbharadwaj101@gmail.com>
2019-08-11 12:57:30 +0530

    Added Document

--- Assignment-6/main.c
+++ Assignment-6/main.c
@@ -15,12 +15,16 @@ int main(){
 		time2 = 0;
 
 	while(!stop){
-		printf("Enter the Job details: ");
-		scanf("%s",tmp.jno);
-		scanf("%d",&tmp.bt);
+
 
 		time1 = getWaitTime(f1,r1);
 		time2 = getWaitTime(f2,r2);
+
+		printf("Current wait time Q1 : %d\n",time1);
+		printf("Current wait time Q2 : %d\n",time2);
+		printf("Enter the Job details: ");
+		scanf("%s",tmp.jno);
+		scanf("%d",&tmp.bt);
 
 		if(time1 <= time2)
 			enqueue(&f1,&r1,tmp.jno,tmp.bt);
@@ -30,13 +34,13 @@ int main(){
 		printf("Do you want to stop 1/0? ");
 		scanf("%d",&stop);
 	}
-
+	printf("\n--------------------------------------\n\n");
 	printf("Queue1:\n");
 	displayQueue(f1,r1);
-	printf("\nAverage Waiting Time in Q1 : %f\n",getAvgWaitTime(f1,r1));
-	printf("--------------------------\n");
+	printf("\nAverage Waiting Time in Q1 : %.3f\n",getAvgWaitTime(f1,r1));
+	printf("----------------------------------------\n");
 	printf("Queue2:\n");
 	displayQueue(f2,r2);
-	printf("\nAverage Waiting Time in Q2 : %f\n",getAvgWaitTime(f2,r2));
+	printf("\nAverage Waiting Time in Q2 : %.3f\n",getAvgWaitTime(f2,r2));
 
 }
